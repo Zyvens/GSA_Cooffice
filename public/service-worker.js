ccbd72832a3a19b61f7bbee45ef978d76111a506
Vitor Ivens <vitorivens@gmail.com>
2026-09-22 01:45:17 -0300

    chore: bump PWA cache for map-first interface

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='gsa-cooffice-v2';
+const CACHE='gsa-cooffice-v3';
 const CORE=['/','/index.html','/style.css','/app.mjs','/map.mjs','/realtime.mjs','/icons.mjs','/shared/world.mjs','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
